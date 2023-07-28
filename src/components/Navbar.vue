f1d8eac2e99ce2e710502efead6f481268668400
Oleh Krupa <krupao980@gmail.com>
2023-07-28 14:03:41 +0300

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                Binance 2.0
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> ('Login') </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> ('Register') </a>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            Auth::user()->first_name
+                            Auth::user()->last_name
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+                            <a class="dropdown-item" href="#">
+                                ('Preferences')
+                            </a>
+
+                            <a class="dropdown-item" href="#" onclick="#">
+                                ('Logout')
+                            </a>
+
+                            <form id="logout-form" action="#" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
