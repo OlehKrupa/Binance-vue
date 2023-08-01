432b64b9c332f639eef823afe4c9d2f9abff7f22
Oleh Krupa <krupao980@gmail.com>
2023-08-01 17:42:21 +0300

    Create PreferencesPage.vue

---
+++ src/pages/PreferencesPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Your Preferences</div>
+                    <CryptoTable/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import CryptoTable from '../components/CryptoTable.vue';
+</script>
