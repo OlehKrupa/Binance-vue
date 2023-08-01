dfb63f1ce4d1313e76e0492725bfc676b6317ed5
Oleh Krupa <krupao980@gmail.com>
2023-08-01 17:42:34 +0300

    Update CryptoTable.vue

--- src/components/CryptoTable.vue
+++ src/components/CryptoTable.vue
@@ -1,51 +1,143 @@
-<!-- CryptoTable.vue -->
 <template>
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Full name</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="currency in currencies" :key="currency.id">
-            <td>{{ currency.name }}</td>
-            <td>{{ currency.full_name }}</td>
-            <td>
-              <img :src="currency.image_url" alt="currency img" style="width: 30px; height: 30px;" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  import { allCurrencies } from '../http/currency-api';
-  
-  export default {
-    data() {
-      return {
-        currencies: [],
-      };
+  <div class="table-container">
+    <table class="currency-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Currency</th>
+          <th>Price</th>
+          <th>Trend</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(currency, index) in currencies" :key="currency.id"
+          :class="{ 'highlighted': isPreferredCurrency(currency.id) }" @click="updateUserPreferences(currency.id)">
+          <td>{{ index + 1 }}</td>
+          <td>
+            <div class="currency-info">
+              <img :src="currency.image_url" alt="currency img" class="currency-image" />
+              <div class="currency-names">
+                <span >{{ currency.full_name }}</span>
+                <span class="short-name">{{ currency.name }}</span>
+              </div>
+            </div>
+          </td>
+          <td>{{ currency.price }}</td>
+          <td>{{ currency.trend }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+import { allCurrencies } from '../http/currency-api';
+import { userPreferences, userPreferencesUpdate } from '../http/user-api';
+
+export default {
+  data() {
+    return {
+      currencies: [],
+      preferences: [],
+    };
+  },
+
+  async beforeMount() {
+    await this.fetchCurrencies();
+    await this.fetchPreferences();
+  },
+
+  methods: {
+    async fetchCurrencies() {
+      try {
+        const response = await allCurrencies();
+        this.currencies = response.data;
+      } catch (error) {
+        console.error('Ошибка', error);
+      }
     },
-  
-    async created() {
-      this.fetchCurrencies();
+
+    async fetchPreferences() {
+      try {
+        const response = await userPreferences();
+        this.preferences = response.data;
+      } catch (error) {
+        console.error('Ошибка', error);
+      }
     },
-  
-    methods: {
-      async fetchCurrencies() {
-        try {
-          const response = await allCurrencies();
-          console.log(response);
-          this.currencies = response.data;
-        } catch (error) {
-          console.error('Error', error);
+
+    isPreferredCurrency(currencyId) {
+      return this.preferences.includes(currencyId);
+    },
+
+    async updateUserPreferences(currencyId) {
+      try {
+        if (this.isPreferredCurrency(currencyId)) {
+          this.preferences = this.preferences.filter(pref => pref !== currencyId);
+        } else {
+          this.preferences.push(currencyId);
         }
-      },
+        const selectedCurrencies = this.preferences;
+        await userPreferencesUpdate({ selectedCurrencies });
+
+      } catch (error) {
+        console.error('Ошибка', error);
+      }
     },
-  };
-  </script>  
+  },
+};
+</script>
+
+
+<style>
+.table-container {
+  margin: 20px;
+}
+
+.currency-table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+}
+
+.currency-table th,
+.currency-table td {
+  padding: 10px;
+  border-bottom: 1px solid #ddd;
+}
+
+.currency-table th {
+  background-color: #f2f2f2;
+}
+
+.currency-table tr:hover {
+  background-color: #f9f9f9;
+}
+
+.currency-image {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+.highlighted {
+  background-color: #cf9;
+}
+
+.currency-info {
+  display: flex;
+  align-items: center;
+}
+
+.currency-names {
+  margin-left: 5px;
+}
+
+.short-name {
+  margin-left: 5px;
+  font-size: 12px;
+  color: #666;
+}
+</style>
