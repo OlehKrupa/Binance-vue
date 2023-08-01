5eaa75e6c04b00c04d836b7ca32990de05ff4aa9
Oleh Krupa <krupao980@gmail.com>
2023-08-01 17:43:32 +0300

    Update DashboardPage.vue

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,14 +1,36 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Your Currencies</div>
-                </div>
-            </div>
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card rounded">
+            <div class="card-header">Your Currencies</div>
+          </div>
         </div>
+      </div>
     </div>
-</template>
-<script>
-
-</script>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style>
+  .container {
+    padding: 20px;
+  }
+  
+  .card {
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+  
+  .card-header {
+    padding: 10px;
+    background-color: #f5f5f5;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    font-weight: bold;
+  }
+  </style>
+  
