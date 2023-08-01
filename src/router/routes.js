449d93a2ce5fb6590ca9d773e8b442af20c181b1
Oleh Krupa <krupao980@gmail.com>
2023-08-01 17:42:16 +0300

    Routes update

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,24 @@ import LoginPage from "../pages/LoginPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
 import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue";
 import DashboardPage from "../pages/DashboardPage.vue";
+import HomePage from "../pages/HomePage.vue";
+import PreferencesPage from "../pages/PreferencesPage.vue";
 
 const routes = [
   {
     path: "/",
+    component: HomePage,
+    name: "homepage",
+  },
+  {
+    path: "/dashboard",
     component: DashboardPage,
     name: "dashboard",
+  },
+  {
+    path: "/preferences",
+    component: PreferencesPage,
+    name: "preferences",
   },
   {
     path: "/login",
