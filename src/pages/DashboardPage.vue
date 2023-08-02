c52f17e4cb2cb68503df9705f6d7f93cd2583547
Oleh Krupa <krupao980@gmail.com>
2023-08-02 17:16:57 +0300

    Add Chart and table

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,36 +1,41 @@
 <template>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card rounded">
-            <div class="card-header">Your Currencies</div>
-          </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card rounded">
+          <div class="card-header">Your Currencies</div>
+          <Chart />
+          <Table />
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script setup>
+
+<script setup>
+import Chart from '../components/Chart.vue';
+import Table from '../components/DashboardTable.vue';
+
+</script>
   
-  </script>
-  
-  <style>
-  .container {
-    padding: 20px;
-  }
-  
-  .card {
-    border: 1px solid #ddd;
-    border-radius: 10px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .card-header {
-    padding: 10px;
-    background-color: #f5f5f5;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    font-weight: bold;
-  }
-  </style>
+<style>
+.container {
+  padding: 20px;
+}
+
+.card {
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+  padding: 10px;
+  background-color: #f5f5f5;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  font-weight: bold;
+}
+</style>
   
