396027517891df9cd7612c7f0caf678a58454252
Oleh Krupa <krupao980@gmail.com>
2023-08-03 13:02:57 +0300

    Update history-api.js

--- src/http/history-api.js
+++ src/http/history-api.js
@@ -5,4 +5,4 @@ const resource = "api/history"
 export const allHistory = () => api.get(resource);
 
 //get 1 currency history on currency id
-export const currencyHistory = () => api.get(`${resource}/${id}`);
+export const currencyHistory = (id) => api.get(`${resource}/${id}`);
