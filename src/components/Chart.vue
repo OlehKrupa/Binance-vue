199690444342b784d7b1dfc2a47b02c5893d86e2
Oleh Krupa <krupao980@gmail.com>
2023-08-04 13:32:09 +0300

    Line chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="chart-container" ref="chart"></div>
+  <div>
+    <div class="chart-container" ref="chart"></div>
+    <div class="select-container text-center">
+      <div class="btn-group">
+        <button v-for="hours in hoursOptions" :key="hours" :class="{ 'btn-selected': selectedHours === hours }"
+          @click="selectHours(hours)">
+          {{ hours >= 24 ? hours / 24 + ' days' : hours + ' Hour' }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,7 +22,9 @@ export default {
     return {
       containerWidth: 0,
       containerHeight: 0,
-      apiData: null, // Initialize with null or an empty array
+      apiData: null,
+      selectedHours: 24,
+      hoursOptions: [1, 12, 24, 48, 72, 168],
     };
   },
   mounted() {
@@ -23,27 +35,40 @@ export default {
   beforeUnmount() {
     window.removeEventListener('resize', this.updateDimensions);
   },
+  watch: {
+    selectedHours() {
+      this.createChart();
+    },
+  },
   methods: {
     async updateDimensions() {
       const chartContainer = this.$refs.chart;
       this.containerWidth = chartContainer.clientWidth;
       this.containerHeight = chartContainer.clientHeight;
-      // When the dimensions change, redraw the chart
       this.createChart();
     },
     async fetchDataFromAPI() {
       try {
-        const response = await currencyHistory(23); // Fetch data for currency ID
-        this.apiData = Object.values(response.data); // Extract array of data objects
+        const response = await currencyHistory(23);
+        this.apiData = Object.values(response.data);
         this.createChart();
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     },
+    calculateNumXTicks(width) {
+      if (width <= 400) {
+        return 4;
+      } else if (width <= 800) {
+        return 6;
+      } else {
+        return 8;
+      }
+    },
     createChart() {
-      if (!this.apiData) return; // If data is not available, do not create the chart
+      if (!this.apiData) return;
 
-      const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+      const margin = { top: 30, right: 30, bottom: 30, left: 60 };
       const width = this.containerWidth - margin.left - margin.right;
       const height = this.containerHeight - margin.top - margin.bottom;
 
@@ -57,48 +82,117 @@ export default {
         .append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
+      const endDate = new Date();
+      const startDate = d3.timeHour.offset(endDate, -this.selectedHours);
+
+      let timeInterval = d3.timeHour;
+      if (this.selectedHours === 1) {
+        timeInterval = d3.timeMinute.every(15);
+      } else if (this.selectedHours === 12) {
+        timeInterval = d3.timeHour;
+      }
+
+      const filteredData = this.apiData.filter((d) => {
+        const date = new Date(d.created_at);
+        return date >= startDate && date <= endDate;
+      });
+
       const x = d3
         .scaleTime()
-        .domain(d3.extent(this.apiData, (d) => new Date(d.created_at)))
+        .domain([startDate, endDate])
         .range([0, width]);
 
-      const yValues = this.apiData.map((d) => +d.buy); // Convert strings to numbers using +
-      const yMidpoint = d3.mean(yValues); // Calculate the midpoint value
-      const yMaxDifference = d3.max(yValues.map((y) => Math.abs(y - yMidpoint))); // Calculate the maximum difference
+      const yValues = filteredData.map((d) => +d.buy);
+      const yMidpoint = d3.mean(yValues);
+      const yMaxDifference = d3.max(yValues.map((y) => Math.abs(y - yMidpoint)));
 
       const y = d3
         .scaleLinear()
-        .domain([yMidpoint - yMaxDifference, yMidpoint + yMaxDifference]) // Set symmetric domain around the midpoint
+        .domain([yMidpoint - yMaxDifference, yMidpoint + yMaxDifference])
         .range([height, 0]);
+
+      const numXTicks = this.calculateNumXTicks(width);
+      const numYTicks = Math.floor(height / 100);
+
+      const xAxis = d3.axisBottom(x).ticks(timeInterval).tickFormat(d3.timeFormat("%b %d, %H:%M")).ticks(numXTicks);
+      const yAxis = d3.axisLeft(y).ticks(numYTicks).tickFormat(d3.format(".2f"));
+
+      svg.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(xAxis)
+        .call(g => g.select(".domain"))
+        .call(g => g.selectAll('.tick line').clone()
+          .attr('y2', -height)
+          .attr('stroke-opacity', 0.2)
+          .attr("stroke-dasharray", "2,2")
+        );
+
+      svg.append('g')
+        .call(yAxis)
+        .call(g => g.select(".domain"))
+        .call(g => g.selectAll('.tick line').clone()
+          .attr('x2', width)
+          .attr('stroke-opacity', 0.2)
+          .attr("stroke-dasharray", "2,2")
+        );
 
       const line = d3
         .line()
+        .defined((d) => !isNaN(+d.buy))
         .x((d) => x(new Date(d.created_at)))
-        .y((d) => y(+d.buy)); // Convert string to number using +
+        .y((d) => y(+d.buy))
+        .curve(d3.curveLinear);
 
       svg.append('path')
-        .datum(this.apiData)
+        .datum(filteredData)
         .attr('fill', 'none')
         .attr('stroke', 'steelblue')
         .attr('stroke-width', 2)
         .attr('d', line);
 
-      svg.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x));
-
-      svg.append('g').call(d3.axisLeft(y));
+      svg.selectAll('.point')
+        .data(filteredData)
+        .enter()
+        .append('circle')
+        .attr('class', 'point')
+        .attr('cx', (d) => x(new Date(d.created_at)))
+        .attr('cy', (d) => y(+d.buy))
+        .attr('r', 3)
+        .attr('fill', 'steelblue');
+    },
+    selectHours(hours) {
+      this.selectedHours = hours;
     },
   },
 };
 </script>
 
 <style>
-/* Add any custom styles here */
 .chart-container {
   width: 100%;
-  /* Make the chart container occupy 100% of the available width */
   height: 400px;
-  /* Set an initial height for the chart */
+}
+
+.select-container {
+  padding: 10px;
+}
+
+.btn-group {
+  display: inline-flex;
+  justify-content: center;
+}
+
+button {
+  cursor: pointer;
+  padding: 5px 10px;
+  margin: 0 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #fff;
+}
+
+.btn-selected {
+  background-color: #007bff;
+  color: #fff;
 }
 </style>
