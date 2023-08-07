2b4e603e83d7cdef6a3a449a71c1a1fe7298f7f2
Oleh Krupa <krupao980@gmail.com>
2023-08-07 18:32:03 +0300

    Bus.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,6 @@ import LoginPage from "../pages/LoginPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
 import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue";
 import DashboardPage from "../pages/DashboardPage.vue";
-import DashboardPageBus from "../pages/DashboardPageBus.vue";
 import HomePage from "../pages/HomePage.vue";
 import PreferencesPage from "../pages/PreferencesPage.vue";
 
@@ -16,11 +15,6 @@ const routes = [
     path: "/dashboard",
     component: DashboardPage,
     name: "dashboard",
-  },
-  {
-    path: "/dashboard_bus",
-    component: DashboardPageBus,
-    name: "dashboard_bus",
   },
   {
     path: "/preferences",
