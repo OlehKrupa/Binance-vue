a6aabf769c043501ae83440f2ffe7cb41119fbf9
Oleh Krupa <krupao980@gmail.com>
2023-08-18 17:26:17 +0300

    Almost work

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,43 +1,42 @@
 <template>
-    <div>
-      <stripe-checkout
-        ref="checkoutRef"
-        mode="subscription"
-        :pk="publishableKey"
-        :line-items="lineItems"
-        :success-url="successURL"
-        :cancel-url="cancelURL"
-        @loading="v => loading = v"
-      />
-      <button @click="submit">Subscribe!</button>
-    </div>
-  </template>
+  <div class="container d-flex justify-content-center mt-5">
+    <stripe-checkout
+    ref="checkoutRef" 
+    :pk="publishableKey" 
+    :sessionId="sessionId"
+    />
+    <button class="btn btn-primary" @click="submit">Pay now!</button>
+  </div>
+</template>
   
-  <script>
-  import { StripeCheckout } from '@vue-stripe/vue-stripe';
-  export default {
-    components: {
-      StripeCheckout,
+<script>
+import { StripeCheckout } from '@vue-stripe/vue-stripe';
+import axios from 'axios';
+
+export default {
+  comsponents: {
+    StripeCheckout
+  },
+  
+  data() {
+    return {
+      publishableKey: 'pk_test_51Ng6pYDtedehY5EN8M2apUVXmZtxuZSXlSXN4DKYfXKy8lt9qfE0eGot3n7Ot0oQKlillBcYluVB7j0iOUx5x8zI00BUNje7fW',
+      sessionId:null
+    }
+  },
+
+  mounted() {
+    this.getSession()
+  },
+
+  methods: {
+    getSession() {
+      axios.get('http://localhost:80/api/getSession').then(res => {this.sessionId = res.data.id}).catch(err => {console.log(err)})
     },
-    data () {
-      this.publishableKey = "pk_test_51Ng6pYDtedehY5EN8M2apUVXmZtxuZSXlSXN4DKYfXKy8lt9qfE0eGot3n7Ot0oQKlillBcYluVB7j0iOUx5x8zI00BUNje7fW";
-      return {
-        loading: false,
-        lineItems: [
-          {
-            price: 'pi_3NgRiGDtedehY5EN0Lvsbp78', // The id of the recurring price you created in your Stripe dashboard
-            quantity: 1,
-          },
-        ],
-        successURL: '#',
-        cancelURL: '#',
-      };
+
+    submit() {
+      this.$refs.checkoutRef.redirectToCheckout();
     },
-    methods: {
-      submit () {
-        // You will be redirected to Stripe's secure checkout page
-        this.$refs.checkoutRef.redirectToCheckout();
-      },
-    },
-  };
-  </script>
+  },
+}
+</script>
